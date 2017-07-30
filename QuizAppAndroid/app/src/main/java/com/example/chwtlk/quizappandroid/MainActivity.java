93f2fcd16c5d70036be7bb36aa4d71e225dcec8c
Disni Charuka <disnicharuka@gmail.com>
2017-07-30 14:30:18 +0530

    GameLoginActivity Created

--- QuizAppAndroid/app/src/main/java/com/example/chwtlk/quizappandroid/MainActivity.java
+++ QuizAppAndroid/app/src/main/java/com/example/chwtlk/quizappandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.chwtlk.quizappandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread splash_Screen = new Thread(){
+
+            public void run(){
+
+                try{
+
+                    sleep(1000);
+
+                }catch(Exception e){
+
+                    e.printStackTrace();
+
+                }finally{
+
+                    startActivity(new Intent(getApplicationContext(),GameLoginActivity.class));
+                    finish();
+                }
+            }
+        };
     }
 }
